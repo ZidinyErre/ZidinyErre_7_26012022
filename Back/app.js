a76c519c7f3d7b61a62686955d79ff7b074692bb
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-01-27 19:56:01 +0100

    connexion serveur base de donnée

--- Back/app.js
+++ Back/app.js
@@ -6,23 +6,27 @@ const app = express();
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "TWVzam9saWVzZmxldXJzYmxldQ==",
+    password: "2525,pasM!",
+    database: "groupomania"
 });
 
 db.connect((err) =>{
     if(err){
-        throw err;
+        console.error('error connecting:' + err.stack)
+        return
     }
     console.log('MySQL connected...');
 });
 
-app.get('/createdb', (req, res) => {
-    let sql = 'CREATE DATABASE groupomania';
-    db.query(sql, (err,result) => {
-        if(err) throw err ;
-        console.log(result);
-        res.send('Database created..')
-    });
-});
+module.exports = db
+//app.get('/createdb', (req, res) => {
+//    let sql = 'CREATE DATABASE groupomania';
+//    db.query(sql, (err,result) => {
+//       if(err) throw err ;
+//        console.log(result);
+//        res.send('Database created..')
+//    });
+//});
+
 
 module.exports = app;
