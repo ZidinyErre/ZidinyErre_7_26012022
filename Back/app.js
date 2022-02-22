d119b3df9630071dd561bae5d73547a0b804f4d5
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-02-22 15:32:54 +0100

    mise en place d'un fichier de connexion global

--- Back/app.js
+++ Back/app.js
@@ -1,26 +1,10 @@
 const express = require('express');
 const mysql = require('mysql');
-
+const db = require('./db');
 
 const app = express();
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "2525,pasM!",
-    database: "groupomania"
-});
 
-
-db.connect((err) =>{
-    if(err){
-        console.error('error connecting:' + err.stack)
-        return
-    }
-    console.log('MySQL connected...');
-});
-
-module.exports = db
 //app.get('/createdb', (req, res) => {
 //    let sql = 'CREATE DATABASE groupomania';
 //    db.query(sql, (err,result) => {
--- Back/db.js
+++ Back/db.js
@@ -1,4 +1,20 @@
 const mysql = require('mysql');
 
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "2525,pasM!",
+    database: "groupomania"
+});
 
+
+db.connect((err) =>{
+    if(err){
+        console.error('error connecting:' + err.stack)
+        return
+    }
+    console.log('MySQL connected...');
+});
+
+module.exports = db
