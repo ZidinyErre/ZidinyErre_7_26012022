f35e05cbb9dfa851c83973c26d472691af14af1e
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-02-24 01:15:47 +0100

    mise en place des midellware

---
+++ Back/middlewares/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+// Middleware qui va permettre d'authentifié l'utilisateur et de le permettre d'agir ou non
+module.exports = (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET );
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw res.status(403).json({error: 'Requête non autorisé'});
+          } else {
+            next();
+        }
+    }catch (error){
+        res.status(401).json({error: error | 'Requête non authentifié !'});
+    }
+};
