4bb174abf1d954eefe704a1fbbdd16c1fabbe570
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-03-02 14:01:29 +0100

    premier version sign up réussi

--- Back/models/user.model.js
+++ Back/models/user.model.js
@@ -41,16 +41,37 @@ const dotenv = require('dotenv');
 //     });
 
 // };
+// exports.signup = (req, res, next) => {
+//     console.log(nom, prenom, email, password, service, role);
+//     const {nom, prenom, email, password, service, role} = req.body
+
+//         db.query(`INSERT INTO user SET (nom, prenom, email, password, service, role) VALUES (?,?,?,?,?,?) `, [nom,prenom,email,password,service,role], (err,res) =>{
+//             if(err) {
+//                 console.log(err);
+//             }
+//             else{
+//                 res.send( 'Profil créé !');
+//             } 
+//         });
+// };
+
 exports.signup = (req, res, next) => {
-        db.query(`INSERT INTO user SET (nom,prenom,email,password,service,role) VALUES (?,?,?,?,?,?) `, [nom,prenom,email,password,service,role], (err,res) =>{
+    const {nom, prenom, email, password, service, role} = req.body
+    const sql = `INSERT INTO user (nom, prenom, email, password, service, role) VALUES (?,?,?,?,?,?)`
+
+        db.query( sql, [nom,prenom,email,password,service,role], (err, result) =>{
             if(err) {
                 console.log(err);
             }
             else{
                 res.send( 'Profil créé !');
+                console.log(result);
+
             } 
         });
 };
+
+
 
 // exports.signup = (req, res, next) => {
     
