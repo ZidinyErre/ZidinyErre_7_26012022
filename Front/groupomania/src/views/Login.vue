0f0ec4673f0c4b352aefcacdf6d662cfbaebebed
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-06-08 18:28:06 +0200

    Travail et compréhension du système Vue

---
+++ Front/groupomania/src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="card text-center">
+        <div class="card-header">
+        Featured
+        </div>
+        <div class="card-body">
+        <div class="input-group input-group-sm mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-sm">Email</span>
+            </div>
+            <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+            </div>
+
+            <div class="input-group input-group-sm mb-3">
+            <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-sm">Mots de passe </span>
+            </div>
+            <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
+            </div>   
+
+            <a href="#" class="btn btn-primary">Connexion</a>
+        </div>
+        <div class="card-footer text-muted">
+        2 days ago
+        </div>
+    </div>
+</template>
+
+<script> 
+</script>
+
+<style>
+.card{
+    width: 35em;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
