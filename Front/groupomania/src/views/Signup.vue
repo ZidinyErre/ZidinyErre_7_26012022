d982fad84690a994e106f510541d805bfb65de9f
Zidiny Erre <mr.erre.zidiny@gmail.com>
2022-06-17 02:04:39 +0200

    Création de user depuis front possible (raffraichissement de la page)

--- Front/groupomania/src/views/Signup.vue
+++ Front/groupomania/src/views/Signup.vue
@@ -12,24 +12,24 @@
                 <form class="row g-3">
                     <div class="col-md-6">
                         <label for="inputName" class="form-label">Nom</label>
-                        <input type="name" class="form-control" id="inputName">
+                        <input type="name"  v-model="nom"  class="form-control" id="inputName">
                     </div>
                     <div class="col-md-6">
                         <label for="inputFirstName" class="form-label">Prénom</label>
-                        <input type="firstname" class="form-control" id="inputFirstName">
+                        <input type="firstname"  v-model="prenom"  class="form-control" id="inputFirstName">
                     </div>
                     <p id="retourLogin" class="form-text">Vous avez déjà un compte? <a @click="goToConnexion" class="linkTo"> Par ici</a></p>
                     <div class="col-12">
                         <label for="inputEmail" class="form-label">E-mail</label>
-                        <input type="email" class="form-control" id="inputEmail" placeholder="@">
+                        <input type="email"  v-model="email"  class="form-control" id="inputEmail" placeholder="@">
                     </div>
                     <div class="col-12">
                         <label for="inputPassword" class="form-label">Mots de passe </label>
-                        <input type="password" class="form-control" id="inputPassword" placeholder="A l'abri des regards indiscret ">
+                        <input type="password"  v-model="password" class="form-control" id="inputPassword" placeholder="A l'abri des regards indiscret ">
                     </div>
                     <div class="col-md-6">
                         <label for="inputService" class="form-label">Service</label>
-                        <input type="text" class="form-control" id="inputService">
+                        <input type="text"  v-model="service"  class="form-control" id="inputService">
                     </div>
                     <!-- <div class="col-md-4">
                         <label for="inputState" class="form-label">Statue</label>
@@ -41,7 +41,7 @@
 
                     
                     <div class="col-12">
-                        <button  @click="createAccount()"  type="submit" class="btn btn-primary">S'inscrire</button>
+                        <button  @click="createAccount()" type="submit" class="btn btn-primary">S'inscrire</button>
                     </div>
                 </form>
             </div>
@@ -55,6 +55,8 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'signup',
     data: function () {
@@ -72,14 +74,15 @@ export default {
         goToConnexion(){
             this.$router.push('/login')
         },
-        createAccount: function(){
-            this.$store.dispatch('createAccount',{
+        createAccount(){
+            axios.post(`http://localhost:3000/api/auth/signup/`,{
                 nom: this.nom, 
                 prenom: this.prenom,
                 email: this.email,
                 password: this.password,
                 service : this.service
             })
+            
         }
     }
 }
